deadbc2d6665e9938b8a848048c62d7b28a528dd
David Vanegas <david-vanegas@javeriana.edu.co>
2020-09-09 21:41:47 -0500

    Añadi un .js para ver como funcionan las variables

---
+++ valores.js
@@ -0,0 +1,14 @@
+var nombre = "David";
+
+var edad;
+edad = 20;
+
+var jugadores;
+jugadores = ["David", "Juan", "Camilo"];
+jugadores
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+persona;
